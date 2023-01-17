75fa837068f135bfbcafb807d6a565188fd56261
Tylor Lee <e92m3gtcsr@gmail.com>
2023-01-17 13:57:45 +0900

    technical experiment project - init

---
+++ a8sb2ws_ex/src/main/java/com/a8sb2ws_ex/boot/A8sb2wsExApplication.java
@@ -0,0 +1,13 @@
+package com.a8sb2ws_ex.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class A8sb2wsExApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(A8sb2wsExApplication.class, args);
+	}
+
+}
---
+++ a8sb2ws_ex/src/test/java/com/a8sb2ws_ex/boot/A8sb2wsExApplicationTests.java
@@ -0,0 +1,13 @@
+package com.a8sb2ws_ex.boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class A8sb2wsExApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
